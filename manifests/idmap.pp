4a0f8a23120adad2c8e29162ced273f5e118c2ba
kakwa <carpentier.pf@gmail.com>
2015-04-12 08:32:30 +0200

    puppet-lint

--- manifests/idmap.pp
+++ manifests/idmap.pp
@@ -37,30 +37,30 @@
 #
 
 define samba::idmap(
-  $domain               = undef,
-  $idrangemin           = undef,
-  $idrangemax           = undef,
-  $backend              = undef,
-  $schema_mode          = undef,
-  $rangesize            = 100000,
-  $read_only            = 'no',
-  $ignore_builtin       = 'no',
-  $name_map             = undef,
-  $ldap_base_dn         = undef,
-  $ldap_user_dn         = undef,
-  $ldap_url             = undef,
-  $ldap_passwd          = undef,
-  $script               = undef,
-) inherits ::samba::params{
+    $domain               = undef,
+    $idrangemin           = undef,
+    $idrangemax           = undef,
+    $backend              = undef,
+    $schema_mode          = undef,
+    $rangesize            = 100000,
+    $read_only            = 'no',
+    $ignore_builtin       = 'no',
+    $name_map             = undef,
+    $ldap_base_dn         = undef,
+    $ldap_user_dn         = undef,
+    $ldap_url             = undef,
+    $ldap_passwd          = undef,
+    $script               = undef,
+    ) inherits ::samba::params{
 
 
   unless is_integer($idrangemin)
     and is_integer($idrangemax)
     and $idrangemin >= 0
     and $idrangemax >= $idrangemin {
-    fail('idrangemin and idrangemax must be integers \
-and idrangemin <= idrangemax')
-  }
+      fail('idrangemin and idrangemax must be integers \
+          and idrangemin <= idrangemax')
+    }
 
   unless $domain{
     fail('domain must be a valid domain or *')
@@ -71,85 +71,90 @@ and idrangemin <= idrangemax')
 
   unless member($checkbackend, downcase($backend)){
     fail("role must be in [${checkbackendstr}]")
-  } 
-  
+  }
+
   $cp = "idmap config ${domain} :"
 
-  case downcase($backend) {
-   'ad': {
-      unless $schema_mode {
-        fail("missing parameter(s) for idmap_${backend}, need: schema_mode")
-      }
-      $idmap_specific = {
-	"${cp} schema_mode" => $schema_mode,
-      }
-   }
-   'autorid': {
-      unless $rangesize and $read_only and $ignore_builtin {
-        fail("missing parameter(s) for idmap_${backend} need: rangesize, read_only, ignore_builtin")
-      }
-      $idmap_specific = {
-	"${cp} rangesize"      => $rangesize,
-	"${cp} read only"      => $read_only,
-	"${cp} ignore builtin" => $ignore_builtin,
-      }
-   }
-   'hash': {
-      unless $name_map{
-        fail("missing parameter(s) for idmap_${backend} need: name_map")
-      }
-      $idmap_specific = {
-        "idmap_hash:name_map" => $name_map,
-      }
-   }
-   'ldap': {
-      unless $ldap_base_dn and $ldap_user_dn and $ldap_url and $ldap_passwd {
-        fail("missing parameter(s) for idmap_${backend} need: ldap_base_dn, ldap_user_dn, ldap_url, ldap_passwd")
-      }
-      $idmap_specific = {
-	"${cp} ldap_base_dn" => $ldap_base_dn,
-	"${cp} ldap_user_dn" => $ldap_user_dn,
-	"${cp} ldap_url"     => $ldap_url,
-      }
-      exec { 'set ldap passwd':
-        cmd     => "/bin/false $ldap_passwd",
-        require => Service['SambaClassic'],
-      }
-      #'ldap_passwd' => $ignore_builtin,
-   }
-   'nss': {
-      $idmap_specific = {}
-   }
-   'rid': {
-      $idmap_specific = {}
-   }
-   'tdb2': {
-      if $script {
+    case downcase($backend) {
+      'ad': {
+        unless $schema_mode {
+          fail("missing parameter(s) for idmap_${backend}, need: schema_mode")
+        }
         $idmap_specific = {
-          "${cp} script" => $script,
+          "${cp} schema_mode" => $schema_mode,
         }
       }
-   }
-   'tdb': {
-      $idmap_specific = {}
-   }
-  }
+      'autorid': {
+        unless $rangesize and $read_only and $ignore_builtin {
+          fail("missing parameter(s) for idmap_${backend} need: \
+                  rangesize, read_only, ignore_builtin")
+        }
+        $idmap_specific = {
+          "${cp} rangesize"      => $rangesize,
+          "${cp} read only"      => $read_only,
+          "${cp} ignore builtin" => $ignore_builtin,
+        }
+      }
+      'hash': {
+        unless $name_map{
+          fail("missing parameter(s) for idmap_${backend} need: name_map")
+        }
+        $idmap_specific = {
+          'idmap_hash:name_map' => $name_map,
+        }
+      }
+      'ldap': {
+        unless $ldap_base_dn and $ldap_user_dn and $ldap_url and $ldap_passwd {
+          fail("missing parameter(s) for idmap_${backend} need: \
+                  ldap_base_dn, ldap_user_dn, ldap_url, ldap_passwd")
+        }
+        $idmap_specific = {
+          "${cp} ldap_base_dn" => $ldap_base_dn,
+          "${cp} ldap_user_dn" => $ldap_user_dn,
+          "${cp} ldap_url"     => $ldap_url,
+        }
+        exec { 'set ldap passwd':
+          cmd     => "/bin/false $ldap_passwd",
+          require => Service['SambaClassic'],
+        }
+#'ldap_passwd' => $ignore_builtin,
+      }
+      'nss': {
+        $idmap_specific = {}
+      }
+      'rid': {
+        $idmap_specific = {}
+      }
+      'tdb2': {
+        if $script {
+          $idmap_specific = {
+            "${cp} script" => $script,
+          }
+        }
+      }
+      'tdb': {
+        $idmap_specific = {}
+      }
+      default: {
+        fail('unsupported idmap backend')
+      }
+    }
 
   $idmap_base = {
     "${cp} backend"     => $backend,
     "${cp} range"       => "${idrangemin}-${idrangemax}",
   }
 
-  $merged = merge($idmap_base, $idmap_specific)
+  $idmapOptions = merge($idmap_base, $idmap_specific)
 
-  $idmapOptionsIndex = prefix(keys($idmapOptions),
-    '[global]')
-  ::samba::option{ $idmapOptionsIndex:
-    options         => $idmapOptions,
-    section         => 'global',
-    require         => Package['SambaClassic'],
-    notify          => Service['SambaClassic'],
-  }
+    $idmapOptionsIndex = prefix(keys($idmapOptions),
+        '[global]')
+    ::samba::option{ $idmapOptionsIndex:
+      options         => $idmapOptions,
+      section         => 'global',
+      require         => Package['SambaClassic'],
+      notify          => Service['SambaClassic'],
+    }
 }
 
 # vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
