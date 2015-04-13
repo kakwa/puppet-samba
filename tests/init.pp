d6d0f1837192af60563f307955a807a92649b86b
kakwa <carpentier.pf@gmail.com>
2015-04-14 01:13:32 +0200

    trying changing unit test

--- tests/init.pp
+++ tests/init.pp
@@ -9,4 +9,56 @@
 # Learn more about module testing here:
 # http://docs.puppetlabs.com/guides/tests_smoke.html
 #
-include samba
+
+class { 'samba::params':
+  sernetRepo => false,
+}
+
+class { '::samba::classic':
+  domain          => 'DC',
+  realm           => 'dc.kakwa.fr',
+  smbname         => 'SMB',
+  adminpassword   => 'qwertyP455',
+  sambaloglevel   => 3,
+  logtosyslog     => true,
+  sambaclassloglevel => {
+    'smb' => 2,
+    'idmap' => 10,
+    'winbind' => 10,
+  },
+  globaloptions   => {
+          'winbind cache time' => 10,
+  },
+  globaloptions           => {
+    'server string' => 'Domain Controler',
+  },
+      globalabsentoptions => [
+    'idmap_ldb:use rfc2307',
+  ],
+}
+
+::samba::idmap { 'Domain DC':
+  domain       => 'DC',
+  idrangemin   => 10000,
+  idrangemax   => 19999,
+  backend      => 'ad',
+  schema_mode  => 'rfc2307',
+}
+
+::samba::idmap { 'Domain *':
+  domain       => '*',
+  idrangemin   => 100000,
+  idrangemax   => 199999,
+  backend      => 'tdb',
+}
+
+::samba::share { 'Test Share':
+  # Mandatory parameters
+  path            => '/srv/test/',
+  # Optionnal parameters
+  options         => {             # * Custom options in section [global]
+    'comment'   => 'My test share that I want',
+    'browsable'     => 'Yes',
+    'read only'     => 'No',
+  },
+}
