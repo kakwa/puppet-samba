1c661a22133203c38d93da49df03c2dd597ddef5
kakwa <carpentier.pf@gmail.com>
2015-09-07 21:26:03 +0200

    lint ppolicy_param

---
+++ manifests/dc/ppolicy_param.pp
@@ -0,0 +1,39 @@
+# Logon script resource
+
+define samba::dc::ppolicy_param(
+  $option,
+  $show_string,
+  $value,
+){
+
+  validate_re(
+    $option,
+    '^--.*$',
+    "option must start with '--' and be \
+a valid 'samba-tool domain passwordsettings' option",
+  )
+  
+  validate_re(
+    $show_string,
+    '^.*:$',
+    "show_string must end with ':' \
+and be the string in 'samba-tool domain passwordsettings show' \
+corresponding to option",
+  )
+
+  unless $value {
+    fail('value must not be empty')
+  }
+
+  exec{"cmd_ppolicy_param ${option}":
+    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+    require => Service['SambaDC'],
+    unless  => "[ \
+\"\$( ${::samba::params::sambacmd} domain passwordsettings show -d 1 | \
+sed 's/${show_string}\ *//p;d' )\" = \
+'${value}' ]",
+    command => "${::samba::params::sambacmd} domain passwordsettings set -d 1 \
+${option}='${value}'",
+  }
+}
+# vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
