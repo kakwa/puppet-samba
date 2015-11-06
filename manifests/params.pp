1cbf62c26c5cc7019ac4ad62f0cff0bdda6471a4
Daniel Fonseca Alves <dalraf@gmail.com>
2015-11-06 10:43:50 -0200

    Defines service smb for ubuntu

--- manifests/params.pp
+++ manifests/params.pp
@@ -39,7 +39,7 @@ class samba::params(
           $smbconffile         = '/etc/samba/smb.conf'
           $krbconffile         = '/etc/krb5.conf'
           $packagepyyaml       = 'python-yaml'
-      }
+      }      
       default: {
           fail('unsupported os')
       }
@@ -70,6 +70,22 @@ class samba::params(
           $packagesambaclient  = 'smbclient'
           $servivesambadc      = 'samba-ad-dc'
           $servivesmb          = 'samba'
+          $servivewinbind      = 'winbind'
+          $sambacmd            = '/usr/bin/samba-tool'
+          $sambaclientcmd      = '/usr/bin/smbclient'
+          $sambaoptsfile       = '/etc/default/samba4'
+          $sambaoptstmpl       = "${module_name}/debian-samba.erb"
+          $smbconffile         = '/etc/samba/smb.conf'
+          $krbconffile         = '/etc/krb5.conf'
+          $packagepyyaml       = 'python-yaml'
+      }
+      'Ubuntu': {
+          $packagesambadc      = 'samba'
+          $packagesambaclassic = 'samba'
+          $packagesambawinbind = 'winbind'
+          $packagesambaclient  = 'smbclient'
+          $servivesambadc      = 'samba-ad-dc'
+          $servivesmb          = 'smb'
           $servivewinbind      = 'winbind'
           $sambacmd            = '/usr/bin/samba-tool'
           $sambaclientcmd      = '/usr/bin/smbclient'
