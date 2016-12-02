4bad981e0971652c64db323989d64f18d795a4d3
kakwa <carpentier.pf@gmail.com>
2016-12-02 23:24:36 +0100

    fixing the test script

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-puppet module install puppetlabs-stdlib --modulepath=`pwd`/../
 
-puppet apply --certname=ad.example.org examples/domain_controller.pp --modulepath=`pwd`/../ --debug
+cd `dirname $0`
+puppet module install puppetlabs-stdlib --modulepath=`pwd`/../../
+
+puppet apply --certname=ad.example.org examples/domain_controller.pp --modulepath=`pwd`/../../ --debug
 
 ret=$?
 
+[ $ret -eq 0 ] || echo "AD step failed"
+
+# some clean up
 apt-get purge -y samba-common
 apt-get autoremove -y
 rm -f /usr/local/bin/additional-samba-tool
