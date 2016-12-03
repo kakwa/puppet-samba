1c15c144e6e24cbc662c32401b0d957f573953c9
kakwa <carpentier.pf@gmail.com>
2016-12-03 01:23:32 +0100

    adding some test on listening ports

--- tests/tests.sh
+++ tests/tests.sh
@@ -2,7 +2,7 @@
 
 cleanup(){
     # some clean up
-    apt-get purge -y samba-common
+    apt-get purge -y samba-common winbind
     apt-get autoremove -y
     rm -f /usr/local/bin/additional-samba-tool
     rm -f /usr/local/bin/smb-create-home.sh
@@ -15,7 +15,6 @@ run(){
     tmp=$?
     [ $tmp -eq 0 ] || echo "$message"
     ret=$(( $ret + $tmp ))
-	cleanup
 }
 
 cd `dirname $0`/..
@@ -23,6 +22,17 @@ puppet module install puppetlabs-stdlib --modulepath=`pwd`/../
 
 run tests/init.pp "classic test failed"
 
+cleanup
+
 run tests/dc.pp "AD test failed"
+
+netstat -apn | grep -q ':389'; ret=$(( $ret + $? ))
+netstat -apn | grep -q ':53';  ret=$(( $ret + $? ))
+netstat -apn | grep -q ':636'; ret=$(( $ret + $? ))
+netstat -apn | grep -q ':464'; ret=$(( $ret + $? ))
+
+[ $tmp -eq 0 ] || echo "AD doesn't have all listening ports"
+
+cleanup
 
 exit $ret
