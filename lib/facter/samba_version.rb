12ea677714f61210db927755f042506039d09f24
kakwa <carpentier.pf@gmail.com>
2017-05-23 22:17:41 +0200

    adding facts for determining the samba version

---
+++ lib/facter/samba_version.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/ruby
+require 'facter'
+
+package = 'samba'
+
+
+distid = Facter.value(:operatingsystem)
+
+case distid
+    when /RedHatEnterprise|CentOS|Fedora|RHEL/
+        if  FileTest.exists?("/usr/bin/yum")
+            version = Facter::Util::Resolution.exec('/usr/bin/yum info samba | sed \'s/Version *: \([0-9\.]\+\)/\1/gp;d\' | head -n 1')
+        end
+    when /Ubuntu|Debian/
+        if  FileTest.exists?("/usr/bin/apt-cache")
+            version = Facter::Util::Resolution.exec('apt-cache show samba | sed \'s/Version:.*:\([0-9\.]\+\).*/\1/gp;d\' | head -n 1')
+        end
+    else
+        version = "0.0.0"
+end
+
+#print distid
+#print version
+
+Facter.add("samba_version") do
+    setcode do
+        version
+    end
+end
+
